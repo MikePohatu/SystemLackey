9de65250e5b79bd2df4cb9a371b472fb1bfc4c9c
MikePohatu <mikepohatu@gmail.com>
2014-12-15 08:22:54 +1300

    xml reader initial

--- lackey_shell/Program.cs
+++ lackey_shell/Program.cs
@@ -8,17 +8,20 @@ namespace SystemLackey.Shell
     {
         static void Main(string[] args)
         {
+           
             Task_WinTaskWorker worker = new Task_WinTaskWorker();
             Task_WinScript task = new Task_WinScript(0, "echo test && pause", false, 5,false);
             Task_WinScript task2 = new Task_WinScript(0, @"@echo off && echo DICK!!! && pause", false, 10, false);
             SystemLackey.IO.XmlHandler handler = new SystemLackey.IO.XmlHandler();
+            Task_WinScript task3 = new Task_WinScript();
+            task3.OpenXml(handler.Read(SystemLackey.IO.IOConfiguration.WorkingPath + @"\dbf3a943-af5b-46d3-84f0-15296ae1a9f0.xml"));
 
             //worker.Run(task);
             //worker.Run(task2);
             //Console.WriteLine(task.ScriptFile);
 
-            handler.Write(SystemLackey.IO.IOConfiguration.WorkingPath + @"\" + task.TaskID + ".xml",task2.GetXml());
-            Console.WriteLine(task.GetXml());
+            //handler.Write(SystemLackey.IO.IOConfiguration.WorkingPath + @"\" + task.TaskID + ".xml",task2.GetXml());
+            Console.WriteLine(task3.GetXml());
             Console.Read();
 
         }
