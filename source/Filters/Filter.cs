b4732cfcd9109f84d2fd2c9f3d64099e5f0a3461
MikePohatu <mikepohatu@gmail.com>
2015-01-16 14:35:42 +1300

    Added GPLv2 license

--- source/Filters/Filter.cs
+++ source/Filters/Filter.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿//    Filter.cs: A definition based on a number of queries. Used to apply requirements
+//               or conditions to a task or step respectively. 
+//    Copyright (C) 2015 Mike Pohatu
+
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; version 2 of the License.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- source/Worker/Tasks/ITask.cs
+++ source/Worker/Tasks/ITask.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿//    ITask.cs: Interface for task objects. Task objects define actual things
+//              to be done on the machine. Tasks are link together to make a Job
+//    Copyright (C) 2015 Mike Pohatu
+
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; version 2 of the License.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+using System;
 using System.Xml.Linq;
 
 namespace SystemLackey.Worker
