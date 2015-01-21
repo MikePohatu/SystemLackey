cc4c8a191e47de20a0468fd42b3aa9a9260bd5bd
MikePohatu <mikepohatu@gmail.com>
2015-01-21 22:10:05 +1300

    added versioning information

--- source/SystemLackey/Properties/AssemblyInfo.cs
+++ source/SystemLackey/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("System Lackey Job Builder")]
+[assembly: AssemblyTitle("System Lackey")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("20road")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("310702aa-d821-4bb9-a0d7-238023c1ffe1")]
+[assembly: Guid("a63dfe6a-a4a6-427b-b4fe-b41bd5e92753")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.1")]
 [assembly: AssemblyFileVersion("0.0.0.1")]
