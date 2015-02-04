1f68104228dcbbc71aa23ce025aba6f36ff48a3c
Mike Pohatu <mikepohatu@gmail.com>
2015-02-04 21:30:47 +1300

    included imessagereceiver.cs and messageforwarder.cs

---
+++ source/core/Messaging/MessageForwarder.cs
@@ -0,0 +1,32 @@
+﻿//    MessageForwarder.cs: Base class for tasks with common methods
+//    Copyright (C) 2015 Mike Pohatu
+
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; version 2 of the License.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+using System;
+using SystemLackey.Messaging;
+using SystemLackey.Worker;
+
+namespace SystemLackey.Messaging
+{
+    public abstract class MessageForwarder : MessageSender, IMessageSender
+    {
+        //Forward any logging messages from the task up the chain
+        protected virtual void ForwardMessage(object o, MessageEventArgs e)
+        {           
+            SendMessage(o, e);
+        }
+    }
+}
